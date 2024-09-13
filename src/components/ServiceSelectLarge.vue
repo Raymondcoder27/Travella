c3a8b510ed6bf641bd9af8aeedc4a8fdbdf074c7
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-13 20:29:31 +0300

    styles service select component

--- src/components/ServiceSelectLarge.vue
+++ src/components/ServiceSelectLarge.vue
@@ -1,7 +1,11 @@
 <template>
     <div id="ServiceSelectLarge">
-        <div class="w-full rounded-2xl">
-            
+        <div class="w-full rounded-2xl bg-gray-100 h-[100px]">
+            <div class="flex justify-between p-2">
+                <div class="text-lg">
+                    Hello World!!
+                </div>
+            </div>
         </div>
     </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,13 @@
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
+import ServiceSelectLarge from '@/components/ServiceSelectLarge.vue';
 </script>
 
 <template>
   <main id="Home" class="flex flex-col h-screen">
     <div class="m-3 mb-auto">
       <div class="grid grid-cols-2 gap-3 my-3">
-
+        <ServiceSelectLarge />
       </div>
     </div>
     <!-- <TheWelcome /> -->
