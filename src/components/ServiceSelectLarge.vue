cdc94e61cf1752486b97409c7f023cbab09ec13b
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-13 20:30:28 +0300

    fixes service select component bugs

--- src/components/ServiceSelectLarge.vue
+++ src/components/ServiceSelectLarge.vue
@@ -2,8 +2,8 @@
     <div id="ServiceSelectLarge">
         <div class="w-full rounded-2xl bg-gray-100 h-[100px]">
             <div class="flex justify-between p-2">
-                <div class="text-lg">
-                    Hello World!!
+                <div class="text-lg ml-2 text-gary-900 mt-14">
+                    Ride
                 </div>
             </div>
         </div>
