30a4f6a217711f94da293c2170b75b8b019c2d6a
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-16 12:03:44 +0300

    init setup

--- src/components/ServiceSelectLarge.vue
+++ src/components/ServiceSelectLarge.vue
@@ -3,10 +3,12 @@
 
     const props = defineProps({
         text: String,
+        imageWidth: String,
         image: String,
+        promo: {Boolean, default:false}
     })
 
-    const { text, image} = toRefs(props)
+    const { text, imageWidth, image} = toRefs(props)
 </script>
 
 <template>
@@ -15,17 +17,14 @@
 
             <div  v-if="promo" class="w-full relative z-10">
                 <span class="absolute bg-green-500 text-gray-100 text-xs rounded-3xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-0.5 px-2">Promo</span>
-                <img width="55" :src="'/public/uber/'+image+'.png'" alt="">
             </div>
-
-
             <div class="flex justify-between p-2">
-                <div class="text-xs text-center mt-2 text-gary-900">
+                <div class="text-lg ml-2 text-gary-900 mt-14">
                     {{text}}
                 </div>
-                <!-- <div class="mt-3 mr-1">
+                <div class="mt-3 mr-1">
                     <img :width="imageWidth" :src="'/public/uber/'+image+'.png'" alt="">
-                </div> -->
+                </div>
             </div>
         </div>
     </div>
--- src/components/ServiceSelectSmall.vue
+++ src/components/ServiceSelectSmall.vue
@@ -3,11 +3,10 @@
 
     const props = defineProps({
         text: String,
-        imageWidth: String,
         image: String
     })
 
-    const { text, imageWidth, image} = toRefs(props)
+    const { text, image} = toRefs(props)
 </script>
 
 <template>
@@ -16,13 +15,12 @@
 
             <div v-if="promo" class="">
                 <div class="absolute top-1/3 left-1/3 transform -translate-x-1/4 -translate-y-1/4 mt-0.5">Promo</div>
+                <img :width="imageWidth" :src="'/public/uber/'+image+'.png'" alt="">
+
             </div>
             <div class="flex justify-between p-2">
-                <div class="text-lg ml-2 text-gary-900 mt-14">
+                <div class="text-xs text-center text-gary-900 mt-2">
                     {{text}}
-                </div>
-                <div class="mt-3 mr-1">
-                    <img :width="imageWidth" :src="'/public/uber/'+image+'.png'" alt="">
                 </div>
             </div>
         </div>
