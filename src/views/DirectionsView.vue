40982ecb8da2414af5ca61814a9ceed4908a057a
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-16 13:35:31 +0300

    init setup

--- src/views/DirectionsView.vue
+++ src/views/DirectionsView.vue
@@ -1,6 +1,21 @@
-<template>
-  <div class="flex justify-center mt-5">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue'
+import ServiceSelectLarge from '@/components/ServiceSelectLarge.vue';
+import ServiceSelectSmall from '@/components/ServiceSelectSmall.vue';
+import MagnifyIcon from 'vue-material-design-icons/Magnify.vue'
+import HomeIcon from 'vue-material-design-icons/Home.vue'
+import MapMarkerIcon from 'vue-material-design-icons/MapMarker.vue'
+</script>
 
+<template>
+  <main id="Directions" class="flex flex-col h-screen">
+
+  </main>
+</template>
+<style scoped>
+/* #ServiceSelectSmall{ */
+    .bg-custom-color {
+        background-color: rgb(237, 237, 237);
+}
+/* } */
+</style>
