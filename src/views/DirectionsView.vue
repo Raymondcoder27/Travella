8ac3d1a1534e3b3f47523026f7612baa75287317
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-16 14:04:30 +0300

    init project setup

--- src/views/DirectionsView.vue
+++ src/views/DirectionsView.vue
@@ -11,7 +11,9 @@
         </div>
 
         <div class="h-16 mt-4 w-full">
-          <div></div>
+          <div :class="[isPickUpActive ? 'circle-black' : 'circle-gray']"></div>
+             <div class="line"></div>
+          <div :class="[!isPickUpActive ? 'square-black' : 'square-gray']"></div>
         </div>
       </div>
     </div>
@@ -19,8 +21,44 @@
 </template>
 <style scoped>
 /* #ServiceSelectSmall{ */
-    .bg-custom-color {
+  .bg-custom-color {
         background-color: rgb(237, 237, 237);
 }
+  .circle-black{
+    margin: 0 auto;
+    width: 9px;
+    height: 9px;
+    background-color: black;
+    border-radius: 100%;
+  }
+
+  .circle-gray{
+    margin: 0 auto;
+    width: 9px;
+    height: 9px;
+    background-color: rgb(191, 191, 191);
+    border-radius: 100%;
+  }
+
+  .square-black{
+    margin: 0 auto;
+    width: 9px;
+    height: 9px;
+    background-color: black;
+  }
+
+  .square-gray{
+    margin: 0 auto;
+    width: 9px;
+    height: 9px;
+    background-color: rgb(191, 191, 191);
+  }
+
+  .line{
+    margin: 0 auto;
+    width: 2px;
+    height: 9px;
+    background-color: rgb(191, 191, 191);
+  }
 /* } */
 </style>
