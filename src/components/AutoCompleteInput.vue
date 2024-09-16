4ca4721c89108e9e8a2ea6d9a487ceea81e2fd1a
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-16 14:31:55 +0300

    init project setup

--- src/components/AutoCompleteInput.vue
+++ src/components/AutoCompleteInput.vue
@@ -1,5 +1,15 @@
 <script>
+    import {toRefs, defineProps, defineEmits} from 'vue'
 
+    const emit = defineEmits(['update:input', 'clearInput', 'isActive'])
+
+    const props = defineProps({
+        theId:String,
+        imageWidth: String,
+        image:String,
+        promo: {Boolean, default:false}
+    })
+    const {text,imageWidth, image} = toRefs(props)
 </script>
 
 <template>
@@ -9,7 +19,8 @@
         @click="emit('isActive', true)" 
         type="text" 
         class="text-md bg-gray-100 appearance-none w-full py-2.5 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:bg-gray-200"
-        placeholder="placeholder">
+        placeholder="placeholder"
+        autocomplete="off">
     </div>
 </template>
 
