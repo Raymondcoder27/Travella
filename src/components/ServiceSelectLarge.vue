8d798ede094dd8606727461f5e1eea135baa112e
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-16 10:09:30 +0300

    init setup

--- src/components/ServiceSelectLarge.vue
+++ src/components/ServiceSelectLarge.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="ServiceSelectLarge">
-        <div class="w-full rounded-2xl bg-gray-100 h-[100px]">
+        <div class="w-full rounded-2xl bg-custom-color h-[100px]">
             <div class="flex justify-between p-2">
                 <div class="text-lg ml-2 text-gary-900 mt-14">
                     Ride
@@ -12,3 +12,9 @@
         </div>
     </div>
 </template>
+
+<style scoped>
+    .bg-custom-color{
+        background-color:rgb(237,237,237)
+    }
+</style>
