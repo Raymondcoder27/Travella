3d13fac7963d3c51e3119280b3387722c042ccda
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-16 14:24:02 +0300

    init setup

--- src/components/AutoCompleteInput.vue
+++ src/components/AutoCompleteInput.vue
@@ -3,8 +3,13 @@
 </script>
 
 <template>
-    <div id="AutoCompleteInput">
-        <input type="text" class="text-md bg-gray-100 appearance-none w-full py-2.5 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+    <div id="AutoCompleteInput" class="flex items-center">
+        <input
+        :id="theId" v-model="inputComputed"
+        @click="emit('isActive', true)" 
+        type="text" 
+        class="text-md bg-gray-100 appearance-none w-full py-2.5 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:bg-gray-200"
+        placeholder="placeholder">
     </div>
 </template>
 
