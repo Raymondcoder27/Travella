ed9cab6f77da644d6d74cd177c6e53b3d905c56b
Raymond Mwebe <122720713+Raymondcoder27@users.noreply.github.com>
2024-10-26 21:34:04 +0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,4 @@
 <script setup>
-
 // import TheWelcome from '../components/TheWelcome.vue'
 import ServiceSelectLarge from '@/components/ServiceSelectLarge.vue';
 import ServiceSelectSmall from '@/components/ServiceSelectSmall.vue';
